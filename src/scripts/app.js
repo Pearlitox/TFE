e8a20508de552e167f481fa89c27cddaf9953236
Pearlitox <151786530+Pearlitox@users.noreply.github.com>
2026-04-18 13:57:29 +0200

    fabricjsinit

---
+++ src/scripts/app.js
@@ -0,0 +1,15 @@
+import { StaticCanvas, FabricText } from 'fabric';
+
+const canvas = new StaticCanvas();
+const helloWorld = new FabricText('Hello world!');
+canvas.add(helloWorld);
+canvas.centerObject(helloWorld);
+
+const imageSrc = canvas.toDataURL();
+// some download code down here
+const a = document.createElement('a');
+a.href = imageSrc;
+a.download = 'image.png';
+document.body.appendChild(a);
+a.click();
+document.body.removeChild(a);
